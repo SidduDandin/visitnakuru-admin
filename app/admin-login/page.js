3b80838c3ba182073a0a73ab8243e25225c5162d
SidduDandin <dandinsiddu@gmail.com>
2025-09-02 19:47:39 +0530

    lastonetime

--- app/admin-login/page.js
+++ app/admin-login/page.js
@@ -1,14 +1,14 @@
-// import { cookies } from "next/headers";
-// import { redirect } from "next/navigation";
-// import LoginForm from "./LoginForm"
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+import LoginForm from "./LoginForm"
 
-// export default function LoginPage() {
-//   // const token = cookies().get("token");
+export default function LoginPage() {
+  const token = cookies().get("token");
 
-//   // // Already logged in → go to admin
-//   // if (token) {
-//   //   redirect("/admin");
-//   // }
+  // Already logged in → go to admin
+  if (token) {
+    redirect("/admin");
+  }
 
-//   return <LoginForm  />;
-// }
+  return <LoginForm  />;
+}
